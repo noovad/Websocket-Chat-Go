ff8d17cb78afe34b3d53515ff636a95e6864409e
Noovad <novadwynt28@gmail.com>
2025-03-23 13:32:14 +0700

    feat: add broadcast

--- main.go
+++ main.go
@@ -8,28 +8,32 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-// Initialize upgrader to upgrade HTTP connections to WebSocket
 var upgrader = websocket.Upgrader{
 	CheckOrigin: func(r *http.Request) bool {
-		return true // Allow all origins (not recommended for production)
+		return true
 	},
 }
 
+var clients = make(map[*websocket.Conn]bool)
+var broadcast = make(chan string)
+
 func main() {
 	http.HandleFunc("/", serveHome)
-	http.HandleFunc("/ws", handleWebSocket)
+	http.HandleFunc("/ws", handleConnections)
+
+	go handleBroadcast()
 
 	fmt.Println("Server running at http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// Function to serve the main page (index.html)
+// Serve the home page
 func serveHome(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "index.html")
 }
 
-// Function to handle WebSocket connections
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+// Handle WebSocket connections
+func handleConnections(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Error upgrading to WebSocket:", err)
@@ -37,23 +41,36 @@ func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	}
 	defer conn.Close()
 
+	clients[conn] = true
 	log.Println("Client connected!")
 
 	for {
-		// Read message from the client
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
 			log.Println("Error reading message:", err)
+			delete(clients, conn)
 			break
 		}
 
-		log.Printf("Message received: %s\n", msg)
+		// Broadcast the received message to the broadcast channel
+		broadcast <- string(msg)
+	}
+}
 
-		// Echo the received message back to the client
-		err = conn.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			log.Println("Error sending message:", err)
-			break
+// Handle broadcasting messages to all clients
+func handleBroadcast() {
+	for {
+		// Retrieve message from the broadcast channel
+		msg := <-broadcast
+
+		// Send the message to all connected clients
+		for client := range clients {
+			err := client.WriteMessage(websocket.TextMessage, []byte(msg))
+			if err != nil {
+				log.Println("Error sending message:", err)
+				client.Close()
+				delete(clients, client)
+			}
 		}
 	}
 }
